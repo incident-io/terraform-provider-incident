1f9b2c8aeaa0294f9a3609c75b519470fab08697
Louis Heath <lheath1998@gmail.com>
2024-04-22 16:39:34 +0100

    unit test

--- internal/provider/incident_workflow_resource_test.go
+++ internal/provider/incident_workflow_resource_test.go
@@ -1,12 +1,13 @@
 package provider
 
 import (
+	"bytes"
+	"reflect"
 	"testing"
 	"text/template"
 
 	"github.com/Masterminds/sprig"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
-	"github.com/incident-io/terraform-provider-incident/internal/client"
 )
 
 func TestAccIncidentWorkflowResource(t *testing.T) {
@@ -16,10 +17,12 @@ func TestAccIncidentWorkflowResource(t *testing.T) {
 		Steps: []resource.TestStep{
 			// Create and check state
 			{
-				Config: overrideAndMarshalModel(incidentWorkflowDefault(), nil),
+				Config: testAccIncidentWorkflowResourceConfig(nil),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttr(
 						"incident_workflow.example", "name", incidentWorkflowDefault().Name),
+					resource.TestCheckResourceAttr(
+						"incident_workflow.example", "condition_groups.0.conditions.0.param_bindings.0.array_value.0.literal", incidentWorkflowDefault().ConditionParam),
 				),
 			},
 			// Import
@@ -28,9 +31,9 @@ func TestAccIncidentWorkflowResource(t *testing.T) {
 				ImportState:       true,
 				ImportStateVerify: true,
 			},
-			// Update and check new state
+			// Update name and check new state
 			{
-				Config: overrideAndMarshalModel(incidentWorkflowDefault(), &client.Workflow{
+				Config: testAccIncidentWorkflowResourceConfig(&workflowTemplateOverrides{
 					Name: "My New Name",
 				}),
 				Check: resource.ComposeAggregateTestCheckFunc(
@@ -38,9 +41,24 @@ func TestAccIncidentWorkflowResource(t *testing.T) {
 						"incident_workflow.example", "name", "My New Name"),
 				),
 			},
+			// Update conditions and check new state
+			{
+				Config: testAccIncidentWorkflowResourceConfig(&workflowTemplateOverrides{
+					ConditionParam: "closed",
+				}),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttr(
+						"incident_workflow.example", "condition_groups.0.conditions.0.param_bindings.0.array_value.0.literal", "closed"),
+				),
+			},
 			// (Clean-up)
 		},
 	})
+}
+
+type workflowTemplateOverrides struct {
+	Name           string
+	ConditionParam string
 }
 
 var incidentWorkflowTemplate = template.Must(template.New("incident_workflow").Funcs(sprig.TxtFuncMap()).Parse(`
@@ -48,11 +66,52 @@ resource "incident_workflow" "example" {
   name               = {{ quote .Name }}
   trigger            = "incident.updated"
   terraform_repo_url = "https://github.com/incident-io/test"
+  condition_groups = [
+	{
+	  conditions = [
+		 {
+			operation = "one_of"
+			param_bindings = [
+			  {
+				 array_value = [
+					{
+					  literal = {{ quote .ConditionParam }}
+					}
+				 ]
+			  }
+			]
+			subject = "incident.status.category"
+		 }
+	  ]
+	}
+ ]
 }
 `))
 
-func incidentWorkflowDefault() client.Workflow {
-	return client.Workflow{
-		Name: "My Test Workflow",
+func incidentWorkflowDefault() workflowTemplateOverrides {
+	return workflowTemplateOverrides{
+		Name:           "My Test Workflow",
+		ConditionParam: "open",
 	}
+}
+
+func testAccIncidentWorkflowResourceConfig(override *workflowTemplateOverrides) string {
+	model := incidentWorkflowDefault()
+
+	// Merge any non-zero fields in override into the model.
+	if override != nil {
+		for idx := 0; idx < reflect.TypeOf(*override).NumField(); idx++ {
+			field := reflect.ValueOf(*override).Field(idx)
+			if !field.IsZero() {
+				reflect.ValueOf(&model).Elem().Field(idx).Set(field)
+			}
+		}
+	}
+
+	var buf bytes.Buffer
+	if err := incidentWorkflowTemplate.Execute(&buf, model); err != nil {
+		panic(err)
+	}
+
+	return buf.String()
 }
