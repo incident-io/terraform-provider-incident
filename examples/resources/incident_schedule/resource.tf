d48d1acb572ebb0c7983d68c983fa2d971b7a523
Rory Malcolm <rorymckenziemalcolm@gmail.com>
2024-05-08 13:55:50 +0100

    Docs example

--- examples/resources/incident_schedule/resource.tf
+++ examples/resources/incident_schedule/resource.tf
@@ -1,71 +1,87 @@
 data "incident_user" "rory" {
-  email = "rory.malcolm@incident.io"
+  id = "01HPFH8T92MPGSQS5C1SPAF4V0"
 }
 
 data "incident_user" "martha" {
-  email = "martha@incident.io"
+  slack_user_id = "U01HJ1J2Z6Z"
 }
 
+# This allows lookups by email, slack user ID, or user ID
 data "incident_user" "roryb" {
   email = "rory@incident.io"
 }
 
-resource "incident_schedule" "testing_my_provider" {
-  name = "Testing Terraform"
+# This is exportable from the incident.io dashboard as a Terraform configuration
+resource "incident_schedule" "primary_on_call" {
+  name = "Primary On-call"
 
-  // from tz database
+  # This is a valid value from the tz database
+  # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
   timezone = "Europe/London"
 
   rotations = [{
-    // A string ID for a schedule rotation
-    id   = "01HPFH8T92MPGSQS5C1SPAF4V0"
+    // A string ID for a schedule rotation, user provided
+    id   = "testing-terraform"
     name = "Testing Terraform"
     versions = [
       {
-        // The date that the handover for this rotation version began
+        # The date that the handover for this rotation version began
+        # Expects an RFC3339 formatted string
         handover_start_at = "2024-05-01T12:54:13Z"
 
+        # Reference the data sources for the users
         users = [
           data.incident_user.martha.id,
         ]
 
+        # The number of concurrent users that can be on-call at the same time for a given
+        # rotation.
         layers = [
           {
             // A string ID for a layer, to be shared across versions if required
-            id   = "01HPFH8T92MPGSQS5C1SPAF4V0"
-            name = "oncall"
+            id   = "primary"
+            name = "Primary"
           }
         ]
 
-        // handovers are optional
         handovers = [
           {
+            # Allowed values are 'daily' and 'weekly' and 'hourly'
             interval_type = "daily"
             interval      = 1
           }
         ]
       },
       {
-        // The date that a schedule rotation versin came into effect
+        # The date that a schedule rotation versin came into effect
+        # Expects an RFC3339 formatted string
         effective_from = "2024-05-14T12:54:13Z"
-        // Used to adjust the handover interval candence - for example, 'changes every week, from the tuesday of that week'
+
+        # Expects an RFC3339 formatted string
+        # Used to adjust the handover interval candence
+        # for example, 'changes every week, from the tuesday of that week'
         handover_start_at = "2024-05-01T12:54:13Z"
 
+        # Reference the data sources for the users
         users = [
           data.incident_user.martha.id,
           data.incident_user.rory.id,
         ]
 
+        # The number of concurrent users that can be on-call at the same time for a given
+        # rotation.
         layers = [
           {
-            id   = "01HPFH8T92MPGSQS5C1SPAF4V0"
-            name = "oncall"
+            // A string ID for a layer, to be shared across versions if required
+            id   = "primary"
+            name = "Primary"
           }
         ]
 
-        // A list of handover intervals, can be used to construct 'changes every week, then every 3 days, then every week'
+        # A list of handover intervals, can be used to construct 'changes every week, then every 3 days, then every week'
         handovers = [
           {
+            # Allowed values are 'daily' and 'weekly' and 'hourly'
             interval_type = "weekly"
             interval      = 1
           }
