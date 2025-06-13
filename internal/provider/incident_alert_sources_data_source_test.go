271931d117e1518aa31d3bb439130d411b2d5337
Anton Leanenka <anton.l@miro.com>
2025-06-13 11:38:31 +0200

    fix docs and examples

--- internal/provider/incident_alert_sources_data_source_test.go
+++ internal/provider/incident_alert_sources_data_source_test.go
@@ -11,87 +11,50 @@ func TestAccIncidentAlertSourcesDataSource(t *testing.T) {
 		PreCheck:                 func() { testAccPreCheck(t) },
 		ProtoV6ProviderFactories: testAccProtoV6ProviderFactories,
 		Steps: []resource.TestStep{
-			// Test retrieving all alert sources
 			{
-				Config: testAccIncidentAlertSourcesDataSourceConfigAll,
+				Config: testAccIncidentAlertSourcesDataSourceConfig,
 				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet("data.incident_alert_sources.all", "alert_sources.#"),
-				),
-			},
-			// Test filtering by source type
-			{
-				Config: testAccIncidentAlertSourcesDataSourceConfigBySourceType,
-				Check: resource.ComposeAggregateTestCheckFunc(
-					resource.TestCheckResourceAttrSet("data.incident_alert_sources.by_source_type", "alert_sources.#"),
-					// All returned sources should be webhook type
-					resource.TestCheckResourceAttr("data.incident_alert_sources.by_source_type", "alert_sources.0.source_type", "webhook"),
+					resource.TestCheckResourceAttr("data.incident_alert_sources.test", "alert_sources.#", "2"),
+					resource.TestCheckResourceAttr("data.incident_alert_sources.test", "alert_sources.0.name", "Test HTTP Alert Source 1"),
+					resource.TestCheckResourceAttr("data.incident_alert_sources.test", "alert_sources.1.name", "Test HTTP Alert Source 2"),
 				),
 			},
 		},
 	})
 }
 
-const testAccIncidentAlertSourcesDataSourceConfigAll = `
-# Create test alert sources
+const testAccIncidentAlertSourcesDataSourceConfig = `
 resource "incident_alert_source" "test1" {
-  name        = "Test Alert Source 1"
+  name        = "Test HTTP Alert Source 1"
   source_type = "http"
-  
   template = {
     title = {
-      literal = "Test Alert 1"
+      literal = "Test Alert Title 1"
     }
     description = {
-      literal = "Test alert description 1"
+      literal = "Test Alert Description 1"
     }
-    attributes = []
-    expressions = []
   }
 }
 
 resource "incident_alert_source" "test2" {
-  name        = "Test Alert Source 2"
+  name        = "Test HTTP Alert Source 2"
   source_type = "http"
-  
   template = {
     title = {
-      literal = "Test Alert 2"
+      literal = "Test Alert Title 2"
     }
     description = {
-      literal = "Test alert description 2"
+      literal = "Test Alert Description 2"
     }
-    attributes = []
-    expressions = []
   }
 }
 
-# Get all alert sources
-data "incident_alert_sources" "all" {
-  depends_on = [incident_alert_source.test1, incident_alert_source.test2]
-}
-`
-
-const testAccIncidentAlertSourcesDataSourceConfigBySourceType = `
-# Create test alert sources with different types
-resource "incident_alert_source" "webhook" {
-  name        = "Test HTTP Alert Source"
+data "incident_alert_sources" "test" {
   source_type = "http"
-  
-  template = {
-    title = {
-      literal = "Test HTTP Alert"
-    }
-    description = {
-      literal = "Test HTTP alert description"
-    }
-    attributes = []
-    expressions = []
-  }
-}
-
-# Get alert sources by source type
-data "incident_alert_sources" "by_source_type" {
-  source_type = "http"
-  depends_on = [incident_alert_source.webhook]
+  depends_on = [
+    incident_alert_source.test1,
+    incident_alert_source.test2
+  ]
 }
 `
