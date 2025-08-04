c3c9717d9d0b9d263cfee4f9486d6b6227c20c03
Ed Stephinson <edward.stephinson@gmail.com>
2025-08-04 16:20:17 +0100

    add default for `ack_mode`

--- internal/provider/incident_escalation_path_resource.go
+++ internal/provider/incident_escalation_path_resource.go
@@ -12,6 +12,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
@@ -218,6 +219,7 @@ func (r *IncidentEscalationPathResource) getPathSchema(depth int) schema.NestedA
 						MarkdownDescription: apischema.Docstring(
 							"EscalationPathNodeLevelV2", "ack_mode"),
 						Optional: true,
+						Default:  stringdefault.StaticString("all"),
 					},
 				},
 			},
